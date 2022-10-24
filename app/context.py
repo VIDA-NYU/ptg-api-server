0d818a85168f1513a6bdcc0c524078a0c00e84ab
Bea Steers <bea.steers@gmail.com>
2022-10-24 15:51:07 -0400

    sketching out multi-db redis connections

--- app/context.py
+++ app/context.py
@@ -9,14 +9,17 @@ import starlette.datastructures
 
 
 def load_config(fname):
-    fname = fname or os.path.join(Context.path, 'config.json')
-    assert os.path.exists(fname)
     with open(fname, 'r') as f:
         config = defaultdict(lambda: None, json.load(f))
 
     # set some defaults
     starlette.datastructures.UploadFile.spool_max_size = int(config.get('spool_max_size') or 0)
     return config
+
+def get_desc(path):
+    f = os.path.join(self.path, '..', 'README.md')
+    desc = (open(f, 'r').read() if os.path.exists(f) else self.config['description'] or '')
+    return desc.split('## Setup Instructions')[0].split('# PTG Data Store API\n')[-1]
 
 class Context:
     path = os.path.dirname(__file__)
@@ -29,7 +32,7 @@ class Context:
     def __init__(self, configFile: str | None=None):
         # self.redis = None
         # self.redisClient = None # Redis connection for client-side support
-        self.config = load_config(configFile)
+        self.config = load_config(configFile or os.path.join(self.path, 'config.json'))
 
     def getDescription(self):
         f = os.path.join(self.path, '..', 'README.md')
@@ -46,4 +49,27 @@ class Context:
         self.redis = await aioredis.from_url(**connection)
         self.redisClient = await aioredis.from_url(**connection, db=1)
         await asyncio.gather(self.redis.ping(), self.redisClient.ping())
+
+
+class Context2:
+    path = os.path.dirname(__file__)
+    def __init__(self, config_fname):
+        self._conns = {}
+        self.config = load_config(config_fname or os.path.join(self.path, 'config.json'))
+
+    async def redis(self, db=0):
+        if db not in self._conns:
+            await self._initialize(db=db)
+        return self._conns[db]
+
+    async def _initialize(self, db=0):
+        connection = self.config['redis']['connection']
+        url = os.getenv('REDIS_URL')
+        if url:
+            connection['url'] = url
+        connection['db'] = db
+        self._conns[db] = await aioredis.from_url(**connection)
+        await self._conns[db].ping()
+
+#ctx = Context2()
 
