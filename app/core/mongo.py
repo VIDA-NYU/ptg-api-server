26a04f7e0ee3fe9715dbba3558a76e7393e86047
Bea Steers <bea.steers@gmail.com>
2022-10-24 15:44:32 -0400

    trigger events on mongodb updates

--- app/core/mongo.py
+++ app/core/mongo.py
@@ -1,6 +1,10 @@
 import os
+import orjson
 import motor.motor_asyncio
 from bson.objectid import ObjectId
+from app.context import Context
+
+ctx = Context.instance()
 
 DEFAULT_MONGO_URI = os.getenv('MONGO_URL') or "mongodb://localhost:27017"
 
@@ -10,10 +14,18 @@ class DB:
         self.client = client = motor.motor_asyncio.AsyncIOMotorClient(uri or DEFAULT_MONGO_URI)
         self.db = db = getattr(client, db or 'app')
         self.collection = db.get_collection(collection)
+        self.collection_name = collection
+
+    async def _pub(self, session):
+        async with ctx.redis.pipeline() as pipe:
+            pipe.xadd(f'event:{self.collection_name}', {b'd': jsondump(session)}, '*', maxlen=10, approximate=True)
+            return await pipe.execute()
 
     def prepare_query(self, _id: str=None, **query):
         if _id:
             query['_id'] = _id#ObjectId(_id)
+        elif 'id' in query:
+            query['_id'] = query.pop('id')
         return query
 
     def process(self, data):
@@ -29,8 +41,9 @@ class DB:
             return self.process(data)
 
     async def add(self, data: dict) -> dict:
-        obj = await self.collection.insert_one(data)
+        obj = await self.collection.insert_one(self.prepare_query(None, **data))
         data = await self.collection.find_one({"_id": obj.inserted_id})
+        await self._pub({'event': 'new', "id": str(obj.inserted_id), 'data': self.process(data)})
         return self.process(data)
 
     async def update(self, _id: str, data: dict) -> bool:
@@ -39,6 +52,7 @@ class DB:
         obj = await self.collection.find_one(self.prepare_query(_id))
         if obj:
             updated = await self.collection.update_one(self.prepare_query(_id), {"$set": data})
+            await self._pub({'event': 'update', 'id': _id, 'data': data})
             return bool(updated)
         return False
 
@@ -46,5 +60,10 @@ class DB:
         obj = await self.collection.find_one(self.prepare_query(_id))
         if obj:
             await self.collection.delete_one(self.prepare_query(_id))
+            await self._pub({'event': 'delete', 'id': _id})
             return True
         return False
+
+def jsondump(data):
+    return orjson.dumps(data, option=orjson.OPT_NAIVE_UTC | orjson.OPT_SERIALIZE_NUMPY)
+
